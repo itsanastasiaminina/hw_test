0749bc0f5ba1c9922099028bfe0d22f3830bbbc1
itsanastasiaminina <itsstacymi@gmail.com>
2023-11-03 00:52:46 +0500

    proj settings

---
+++ generate_arr.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def generate_array():
+    print(np.random.uniform(low=-100, high=100, size=(4, 2)))
---
+++ main.py
@@ -0,0 +1,9 @@
+from greet import print_hi
+from generate_arr import generate_array
+
+name = input()
+
+print_hi(name)
+generate_array()
+
+
